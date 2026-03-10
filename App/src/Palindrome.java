f8c2f700114b9754e8ef5e3b0ed86cd024805202
anmol0917 <anmolsingh091705@gmail.com>
2026-03-10 08:52:28 +0530

    anmol added uc1

---
+++ App/src/Palindrome.java
@@ -0,0 +1,7 @@
+public class Palindrome {
+       public  static void main(String[] args) {
+            System.out.println("Welcome to the Palindrome Checker Management Systsem");
+            System.out.println("Version : 1.0  ");
+            System.out.println("System Initialized Successfully ");
+        }
+    }
